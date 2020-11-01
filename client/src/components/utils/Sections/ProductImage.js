7783648f69b6b6eb3b38c65796bd69cf33427b2a
habib2013 <nairabet25@gmail.com>
2020-11-01 08:51:57 +0100

    --- Auto-Git Commit ---

--- client/src/components/utils/Sections/ProductImage.js
+++ client/src/components/utils/Sections/ProductImage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import ImageGallery from 'react-image-gallery'
+
 
 function ProductImage(props) {
+
+      const [Images, setImages] = useState([])
+
+   useEffect(() => {
+      if(props.detail.image && props.detail.image.length > 0){
+            let images = [];
+
+            props.detail.image && props.detail.image.map(item => {
+               images.push({
+                  original: `http://localhost:5000/${item}`,
+                  thumbnail: `http://localhost:5000/${item}`
+               })
+            })
+
+            setImages(images)
+      }
+   }, [props.detail])
+
    return (
       <div>
-         ProductImage 
+         <ImageGallery
+            items={Images}
+         /> 
       </div>
    )
 }
